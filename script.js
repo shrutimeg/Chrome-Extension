250bf250499f22e20a5930255aa83938d67adf25
Shruti <shrutikiran75@gmail.com>
2023-04-21 17:05:22 +0530

    chrome extension

---
+++ script.js
@@ -0,0 +1,10 @@
+setInterval(function (){
+    
+    var skipButton = document.getElementsByClassName("ytp-ad-skip-button");
+
+    if(skipButton != undefined && skipButton.length > 0) {
+        console.log("Ad detected");
+        skipButton[0].click();
+        
+    }
+}, 5000)   
